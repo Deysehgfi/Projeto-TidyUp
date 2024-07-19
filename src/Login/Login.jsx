186101f303dbc917927bbeb3bb75aef8809d743c
Deyse <138785041+Deysehgfi@users.noreply.github.com>
2024-07-19 12:29:31 -0300

    Add files via upload

---
+++ src/Login/Login.jsx
@@ -0,0 +1,12 @@
+import ImageHome from '../image/Ilustration/img-LoginHome.png'
+import {ImgHomeStyle } from '../styled/Login'
+
+const Login = () => {
+    return(
+        <>
+        <ImgHomeStyle  src={ImageHome} alt="Dois colaboradoes da limpeza higienizando o ambiente." />
+        </>
+    )
+}
+
+export default Login;
---
+++ src/styled/Login.js
@@ -0,0 +1,24 @@
+import { styled } from "styled-components";
+
+export const Cores = {
+    cianoMedio:'#006666',
+    cianoClaro:'#008584',
+    branco:'#f5f5f5',
+    cinza: '#cccccc',
+    cianoEscuro:'#005555',
+    cianoExtraEscuro: '#004141'
+
+
+}
+
+export const ImgHomeStyle = styled.img`
+max-width: 900px;
+max-height: 580px;
+@media screen and (max-width:770px){
+   max-width: 340px;
+   max-height: 220px ;
+  }
+`
+export const Formulario = styled.div`
+
+`
