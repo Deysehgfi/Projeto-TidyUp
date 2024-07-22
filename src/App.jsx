b2291f5920efd2250847e468a93cb6c13c1c072c
Deyse <138785041+Deysehgfi@users.noreply.github.com>
2024-07-22 12:35:03 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Login from "./Login/Login";
+import Section02 from "./Setion02/Section02";
 
 const App = () => {
   return(
@@ -8,7 +9,9 @@ const App = () => {
 
     <Header/>
     <Login/>
+    <Section02/>
     <Footer/>
+    
     
     </>
   )
